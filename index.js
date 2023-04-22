e1c0ecc573ef4411930a024f8f883cde272ca857
Shahzaib786 <125633854+shahzaibee@users.noreply.github.com>
2023-04-22 15:03:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function countdown(endDate) {
+    const intervalId = setInterval(() => {
+        const now = new Date().getTime();
+        const timeRemaining = endDate.getTime() - now;
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+        console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+        if (timeRemaining <= 0) {
+            clearInterval(intervalId);
+            console.log('Countdown ended');
+        }
+    }, 1000);
+}
+// Example usage
+const endDate = new Date('2023-05-01T00:00:00Z');
+countdown(endDate);
+export {};
